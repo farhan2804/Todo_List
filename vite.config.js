6f83b3fafe49db4265457a4bf8f6825befaba376
farhan <el3317.farhanmahmood@gmail.com>
2023-09-08 14:16:23 +0530

    changed the deploy files

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: '/',
-  }
-
-  if (command !== 'serve') {
-    config.base = '/list/'
-  }
-
-  return config
+export default defineConfig({
+  base: '/list/',
+  plugins: [react()],
 })
 
 // import { defineConfig } from 'vite'
